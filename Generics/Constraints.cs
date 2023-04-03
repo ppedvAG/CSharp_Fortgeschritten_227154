b0ced79c3abcac9fa832df0a34fac15d91e8277d
ppedvLukasK <lukask@ppedv.de>
2023-04-03 15:24:02 +0200

    M004

---
+++ Generics/Constraints.cs
@@ -0,0 +1,38 @@
+﻿namespace Generics;
+
+internal class Constraints
+{
+	static void Main(string[] args)
+	{
+
+	}
+
+	public class DataStore1<T> where T : class { } //T muss ein Referenztyp sein (string, Objekte, ...)
+
+	public class DataStore2<T> where T : struct { } //T muss ein Wertetyp sein (int, bool, ...)
+
+	public class DataStore3<T> where T : Constraints { } //T muss von Constraints erben oder selbst die Klasse sein
+														 
+	public class DataStore4<T> where T : new() { } //T muss einen Standardkonstruktor haben
+
+	public class DataStore5<T> where T : Enum { } //T muss ein Enum sein (aber kein Enumwert)
+
+	public class DataStore6<T> where T : Delegate { } //T muss ein Delegate sein
+
+	public class DataStore7<T> where T : unmanaged { }
+	//https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/unmanaged-types
+
+	public class DataStore8<T> where T : class, Enum, new() { } //Mehrere Constraints auf ein Generic
+
+	public class DataStore9<T1, T2> //Mehrere Constraints auf mehrere Generics
+		where T1 : class
+		where T2 : unmanaged
+	{
+	
+	}
+
+	public void Test<MyType>() where MyType : struct //Constraints bei Methode hinzufügen
+	{
+
+	}
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace Generics;
+
+internal class Generics
+{
+	static void Main(string[] args)
+	{
+		List<string> list = new List<string>(); //Generic: T wird nach unten übernommen (hier T = string)
+		list.Add("123");
+
+		List<int> ints = new(); //T wird durch int ersetzt
+		ints.Add(123); //Add(T) -> Add(int)
+
+		Dictionary<string, int> dict = new Dictionary<string, int>(); //Klasse mit 2 Generics: TKey -> string, TValue -> int
+		dict.Add("123", 123); //Add(TKey, TValue) -> Add(string, int)
+	}
+}
+
+public class DataStore<T> :
+	IProgress<T>, //T bei Vererbung weitergeben
+	IEquatable<int> //T statt fixer Typ
+{
+	public T[] data { get; set; } //T bei einem Feld/Property
+
+	public List<T> Data => data.ToList(); //Generic nach unten weitergeben
+
+	public void Add(int index, T item) //T bei Parameter
+	{
+		data[index] = item;
+	}
+
+	public T Get(int index) //T als Rückgabewert
+	{
+		if (index < 0 || index >= data.Length)
+			return default(T); //default(T): Standardwert von T (int: 0, string: null, bool: false, ...)
+		return data[index];
+	}
+
+	public void Report(T value) //T kommt von Interface
+	{
+
+	}
+
+	public bool Equals(int other) //Hier kommt der fixe Typ von oben
+	{
+		return true;
+	}
+
+	public void PrintType<MyType>()
+	{
+		Console.WriteLine(default(MyType)); //default(T): Standardwert von T (int: 0, string: null, bool: false, ...)
+		Console.WriteLine(typeof(MyType)); //Typ von dem Generic
+		Console.WriteLine(nameof(MyType)); //Typ als string (z.B.: "int")
+	}
+}
+
+public class DataStore2<T> : DataStore<T>
+{
+
+}
